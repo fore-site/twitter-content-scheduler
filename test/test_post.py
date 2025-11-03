53cbf12c1b5aa6b917439e4a4fdcc50f24f02a0e
Ogunsanu Oluwaseun <ogunsanu17@gmail.com>
2025-11-03 19:09:14 +0100

    test post update feature

--- test/test_post.py
+++ test/test_post.py
@@ -1,4 +1,7 @@
-from routes.post import make_post
+from routes.post import make_post, post_update
 
 def test_make_post():
     assert make_post is not None
+
+def test_post_update():
+    assert post_update is not None
--- test/test_post_services.py
+++ test/test_post_services.py
@@ -1,4 +1,7 @@
-from services.post import create_post
+from services.post import create_post, update_post
 
 def test_create_post():
     assert create_post is not None
+
+def test_update_post():
+    assert update_post is not None
